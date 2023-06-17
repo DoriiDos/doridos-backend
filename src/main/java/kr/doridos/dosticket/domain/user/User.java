d35fd0f88f055ef3037b85d3dd0daabccff58dc9
jung min jun <alswns7984@naver.com>
2023-06-18 05:20:39 +0900

    feat: User Entity 작성 (#3)

---
+++ src/main/java/kr/doridos/dosticket/domain/user/User.java
@@ -0,0 +1,62 @@
+package kr.doridos.dosticket.domain.user;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "`user`")
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
+
+    @Column(nullable = true)
+    private LocalDateTime deleteAt;
+
+    public User(final String email, final String password, final String nickname,
+                final String phoneNumber, final UserType userType) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.userType = userType;
+    }
+
+    public static User of(final String email, final String password, final String nickname,
+                          final String phoneNumber, final UserType userType) {
+        return new User(email, password, nickname, phoneNumber, userType);
+    }
+}
