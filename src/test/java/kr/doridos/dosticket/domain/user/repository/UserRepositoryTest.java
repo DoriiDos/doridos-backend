d2a58a702f8432d7a263d6a00bf1bb9ad4de36f4
jung min jun <alswns7984@naver.com>
2023-06-22 23:14:39 +0900

    test:  테스트에 auditing 기능 적용 (#3)
    JpaDataTest에 auditing 기능을 사용하기 위해 @Import(JpaAuditingConfig.class)를 추가해주었다.

--- src/test/java/kr/doridos/dosticket/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/doridos/dosticket/domain/user/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package kr.doridos.dosticket.domain.user.repository;
 
+import kr.doridos.dosticket.config.JpaAuditingConfiguration;
 import kr.doridos.dosticket.domain.user.User;
 import kr.doridos.dosticket.domain.user.UserType;
 
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(JpaAuditingConfiguration.class)
 class UserRepositoryTest {
 
     @Autowired
