36c3322774f925f42f3239b68b404e6ef526b628
jung min jun <alswns7984@naver.com>
2023-06-28 19:30:10 +0900

    feat: jwt 토큰 발급기능 구현 (#9)

---
+++ src/main/java/kr/doridos/dosticket/domain/auth/support/jwt/JwtFilter.java
@@ -0,0 +1,41 @@
+package kr.doridos.dosticket.domain.auth.support.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    public JwtFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String token = jwtProvider.resolveToken(request);
+
+        if (token != null && jwtProvider.validateToken(token)) {
+            String email = jwtProvider.getUserEmail(token);
+            setAuthentication(email);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public void setAuthentication(String email) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = jwtProvider.getAuthentication(email);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+}
