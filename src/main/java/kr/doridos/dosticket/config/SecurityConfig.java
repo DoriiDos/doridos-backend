59e78db0736a8d3651f16f6005efbf973a78e53d
jung min jun <alswns7984@naver.com>
2023-06-28 19:28:46 +0900

    feat: security 설정 추가 (#9)

---
+++ src/main/java/kr/doridos/dosticket/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package kr.doridos.dosticket.config;
+
+import kr.doridos.dosticket.domain.auth.support.jwt.CustomAccessDeniedHandler;
+import kr.doridos.dosticket.domain.auth.support.jwt.CustomAuthenticationEntryPoint;
+import kr.doridos.dosticket.domain.auth.support.jwt.JwtFilter;
+import kr.doridos.dosticket.domain.auth.support.jwt.JwtProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    public SecurityConfig(final JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .headers().frameOptions().sameOrigin().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                    .antMatchers("/users/**").permitAll()
+                    .antMatchers("/auth/**").permitAll()
+                    .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+
+                .and()
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
