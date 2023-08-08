e086d6240387f32e6c5970d960f822a2e6d670cb
jung min jun <alswns7984@naver.com>
2023-08-09 01:46:22 +0900

    feat: 티켓정보 수정기능 구현 (#19)

--- src/main/java/kr/doridos/dosticket/domain/ticket/controller/TicketManagerController.java
+++ src/main/java/kr/doridos/dosticket/domain/ticket/controller/TicketManagerController.java
@@ -2,13 +2,11 @@ package kr.doridos.dosticket.domain.ticket.controller;
 
 import kr.doridos.dosticket.domain.auth.support.jwt.UserDetailsImpl;
 import kr.doridos.dosticket.domain.ticket.dto.TicketCreateRequest;
+import kr.doridos.dosticket.domain.ticket.dto.TicketUpdateRequest;
 import kr.doridos.dosticket.domain.ticket.service.TicketManagerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -26,5 +24,13 @@ public class TicketManagerController {
     public ResponseEntity<Void> createTicket(@RequestBody final TicketCreateRequest request, @AuthenticationPrincipal final UserDetailsImpl userDetails) {
         final Long ticketId = ticketManagerService.createTicket(request, userDetails.getUser());
         return ResponseEntity.created(URI.create("/tickets" + ticketId)).build();
+    }
+
+    @PatchMapping("/tickets/{ticketId}")
+    public ResponseEntity<Void> updateTicket(@PathVariable("ticketId") final Long ticketId,
+                                             @RequestBody final TicketUpdateRequest request,
+                                             @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        ticketManagerService.updateTicket(ticketId, request, userDetails.getUser());
+        return ResponseEntity.noContent().build();
     }
 }
