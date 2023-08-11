c2dd06bc1a5043a43fa9bfdb1cd28ecd3920fd1c
minjun <alswns7984@naver.com>
2023-08-11 23:43:48 +0900

    feat: 장소 좌석 더미데이터 추가 (#21)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,4 +7,12 @@ insert into category(id, name, parent_id) values(6, '연극', 6);
 insert into category(id, name, parent_id) values(7, '전시', 7);
 
 insert into place (id, name, location, create_at, update_at) values(1, '잠실경기장', '서울특별시 잠실', now(), now());
-
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(1, 'A', 1, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(2, 'A', 2, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(3, 'A', 3, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(4, 'A', 4, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(5, 'A', 5, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(6, 'B', 1, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(7, 'B', 2, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(8, 'B', 3, 1, now(), now());
+insert into seat(id, section, seat_num, place_id, create_at, update_at) values(9, 'B', 4, 1, now(), now());
