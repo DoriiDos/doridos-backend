3d5038d7e07ad16cd1a60ac0e7fb8123b720f4c8
minjun <alswns7984@naver.com>
2023-08-11 23:44:47 +0900

    feat: 중복되는 시간이 있는지 검증하는 쿼리 추가 (#21)

---
+++ src/main/java/kr/doridos/dosticket/domain/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,19 @@
+package kr.doridos.dosticket.domain.schedule.repository;
+
+import kr.doridos.dosticket.domain.schedule.entity.Schedule;
+import kr.doridos.dosticket.domain.ticket.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("SELECT COUNT(s) FROM Schedule s " + "WHERE s.startTime <= :endTime AND s.endTime >= :startTime " +
+            "AND s.ticket = :ticket")
+    int getSchedulesNumByStartTime(@Param("startTime") LocalDateTime startTime,
+                                   @Param("endTime") LocalDateTime endTime,
+                                   @Param("ticket") Ticket ticket);
+
+}
