2674c9db4f16286f0851e32943adedfdd8f560e1
jung min jun <alswns7984@naver.com>
2024-06-27 19:00:32 +0900

    feat: User 빌더 어노테이션 추가 (#40)

--- src/main/java/kr/doridos/dosticket/domain/user/User.java
+++ src/main/java/kr/doridos/dosticket/domain/user/User.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "`user`")
 @Getter
+@Builder
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
