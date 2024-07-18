0c8879b7221b2d4ec39dec81f070e5bcb464c976
jung min jun <alswns7984@naver.com>
2024-07-18 19:14:25 +0900

    feat: 티켓 조회 기능 구현 (#44)

---
+++ src/main/java/kr/doridos/dosticket/domain/ticket/controller/TicketController.java
@@ -0,0 +1,26 @@
+package kr.doridos.dosticket.domain.ticket.controller;
+
+import kr.doridos.dosticket.domain.ticket.dto.TicketInfoResponse;
+import kr.doridos.dosticket.domain.ticket.service.TicketService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(final TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping("/{ticketId}")
+    public ResponseEntity<TicketInfoResponse> ticketInfo(@PathVariable final Long ticketId) {
+        final TicketInfoResponse response = ticketService.ticketInfo(ticketId);
+        return ResponseEntity.ok(response);
+    }
+}
