c314e4d20755720afad3f21557305c82e20183d0
jung min jun <alswns7984@naver.com>
2024-07-20 22:07:10 +0900

    feat: 티켓 페이징 조회기능 구현 (#46)

--- src/main/java/kr/doridos/dosticket/domain/ticket/controller/TicketController.java
+++ src/main/java/kr/doridos/dosticket/domain/ticket/controller/TicketController.java
@@ -1,7 +1,11 @@
 package kr.doridos.dosticket.domain.ticket.controller;
 
 import kr.doridos.dosticket.domain.ticket.dto.TicketInfoResponse;
+import kr.doridos.dosticket.domain.ticket.dto.TicketPageResponse;
 import kr.doridos.dosticket.domain.ticket.service.TicketService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,5 +26,11 @@ public class TicketController {
     public ResponseEntity<TicketInfoResponse> ticketInfo(@PathVariable final Long ticketId) {
         final TicketInfoResponse response = ticketService.ticketInfo(ticketId);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TicketPageResponse>> findAllTicketsWithPaging(
+            @PageableDefault(size = 10, page = 0) final Pageable pageable) {
+        return ResponseEntity.ok(ticketService.findAllTickets(pageable));
     }
 }
