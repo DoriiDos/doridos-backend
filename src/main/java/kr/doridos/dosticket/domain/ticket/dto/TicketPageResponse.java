23ee7642f2f97f501a17ef102f15fe7051dea9a6
jung min jun <alswns7984@naver.com>
2024-07-30 17:49:45 +0900

    feat: 카테고리별 티켓 검색 기능 구현 (#50)

--- src/main/java/kr/doridos/dosticket/domain/ticket/dto/TicketPageResponse.java
+++ src/main/java/kr/doridos/dosticket/domain/ticket/dto/TicketPageResponse.java
@@ -1,28 +1,45 @@
 package kr.doridos.dosticket.domain.ticket.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import kr.doridos.dosticket.domain.ticket.entity.Ticket;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 public class TicketPageResponse {
 
-    private Long id;
-    private String title;
-    private String content;
-    private String runningTime;
-    private LocalDateTime openDate;
-    private LocalDateTime endDate;
-    private LocalDateTime startDate;
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String runningTime;
+    private final LocalDateTime openDate;
+    private final LocalDateTime endDate;
+    private final LocalDateTime startDate;
+
+    @QueryProjection
+    public TicketPageResponse(Long id,
+                              String title,
+                              String content,
+                              String runningTime,
+                              LocalDateTime openDate,
+                              LocalDateTime endDate,
+                              LocalDateTime startDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.runningTime = runningTime;
+        this.openDate = openDate;
+        this.endDate = endDate;
+        this.startDate = startDate;
+    }
+
 
     public static TicketPageResponse convertToDto(Ticket ticket) {
         return new TicketPageResponse(
                 ticket.getId(),
                 ticket.getTitle(),
-                ticket.getRunningTime(),
+                ticket.getContent(),
                 ticket.getRunningTime(),
                 ticket.getOpenDate(),
                 ticket.getEndDate(),
--- src/main/java/kr/doridos/dosticket/domain/ticket/repository/TicketRepository.java
+++ src/main/java/kr/doridos/dosticket/domain/ticket/repository/TicketRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long>, TicketCustomRepository {
 
     Page<Ticket> findAll(Pageable pageable);
 
