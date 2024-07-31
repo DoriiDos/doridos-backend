4ea8141568d14ef18338c2d50d74cb4ed9bd3786
jung min jun <alswns7984@naver.com>
2024-07-31 18:05:41 +0900

    fix: 토큰 없이 응답보낼경우 응답메시지 내려주도록 변경(#54)

--- src/main/java/kr/doridos/dosticket/domain/auth/support/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/kr/doridos/dosticket/domain/auth/support/jwt/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,9 @@
 package kr.doridos.dosticket.domain.auth.support.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.doridos.dosticket.exception.ErrorCode;
+import kr.doridos.dosticket.exception.ErrorResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -12,6 +16,16 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        setErrorResponse(response, ErrorCode.TOKEN_NOT_FOUND.getMessage(), ErrorCode.TOKEN_NOT_FOUND);
+    }
+
+    public void setErrorResponse(HttpServletResponse response, String message, ErrorCode errorCode) throws IOException {
+        response.setStatus(errorCode.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
