4b1c47c73dbccdf90c1e6d8c7cc59ce7b03415bf
jung min jun <alswns7984@naver.com>
2024-08-19 23:12:31 +0900

    fix: @TestContainers 어노테이션 추가 (#67)

--- src/test/java/kr/doridos/dosticket/domain/reservation/controller/ReservationControllerTest.java
+++ src/test/java/kr/doridos/dosticket/domain/reservation/controller/ReservationControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
+@Testcontainers
 @SpringBootTest
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
