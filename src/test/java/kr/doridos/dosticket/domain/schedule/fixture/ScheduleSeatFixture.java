496904b123c5ee8341e3678ed8bae404aeef8f71
jung min jun <alswns7984@naver.com>
2024-08-19 22:06:38 +0900

    test: 티켓 예매 실패 테스트 케이스를 위한 fixture 추가 (#67)

--- src/test/java/kr/doridos/dosticket/domain/schedule/fixture/ScheduleSeatFixture.java
+++ src/test/java/kr/doridos/dosticket/domain/schedule/fixture/ScheduleSeatFixture.java
@@ -19,4 +19,12 @@ public class ScheduleSeatFixture {
                 .schedule(ScheduleFixture.스케줄_생성())
                 .build();
     }
+
+    public static ScheduleSeat 예약된_좌석생성() {
+        return ScheduleSeat.builder()
+                .id(3L)
+                .isReserved(true)
+                .schedule(ScheduleFixture.스케줄_생성())
+                .build();
+    }
 }
