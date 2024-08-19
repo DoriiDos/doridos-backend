2be990938aebaea3e81e3a07d026df514582ea33
jung min jun <alswns7984@naver.com>
2024-08-19 23:57:37 +0900

    fix: @Configuration 어노테이션 추가 (#67)
    TestContainer 어노테이션에서 변경

--- src/test/java/kr/doridos/dosticket/redis/RedisTestContainer.java
+++ src/test/java/kr/doridos/dosticket/redis/RedisTestContainer.java
@@ -1,11 +1,11 @@
 package kr.doridos.dosticket.redis;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
+@Configuration
 public class RedisTestContainer {
 
     private static final String REDIS_IMAGE = "redis:7.0.8-alpine";
