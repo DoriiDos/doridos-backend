b99ea225396d3548509bac2a7697f1703639abf7
jung min jun <alswns7984@naver.com>
2024-08-22 00:44:14 +0900

    feat: 예매 상세 내역 조회 querydsl 추가 (#71)

--- src/main/java/kr/doridos/dosticket/domain/reservation/repository/ReservationCustomRepositoryImpl.java
+++ src/main/java/kr/doridos/dosticket/domain/reservation/repository/ReservationCustomRepositoryImpl.java
@@ -1,13 +1,20 @@
 package kr.doridos.dosticket.domain.reservation.repository;
 
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.doridos.dosticket.domain.reservation.dto.QReservationInfoResponse;
 import kr.doridos.dosticket.domain.reservation.dto.QReservationResponse;
+import kr.doridos.dosticket.domain.reservation.dto.ReservationInfoResponse;
 import kr.doridos.dosticket.domain.reservation.dto.ReservationResponse;
 import kr.doridos.dosticket.domain.reservation.entity.QReservation;
+import kr.doridos.dosticket.domain.reservation.entity.Reservation;
 import kr.doridos.dosticket.domain.schedule.entity.QSchedule;
+import kr.doridos.dosticket.domain.schedule.entity.ScheduleSeat;
 import kr.doridos.dosticket.domain.ticket.entity.QTicket;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReservationCustomRepositoryImpl implements ReservationCustomRepository{
 
@@ -35,5 +42,34 @@ public class ReservationCustomRepositoryImpl implements ReservationCustomReposit
                 .leftJoin(qSchedule).on(qReservation.scheduleId.eq(qSchedule.id))
                 .where(qReservation.userId.eq(userId))
                 .fetch();
+    }
+
+    @Override
+    public ReservationInfoResponse findByReservationInfo(Reservation reservation) {
+        QTicket qTicket = QTicket.ticket;
+        QSchedule qSchedule = QSchedule.schedule;
+        QReservation qReservation = QReservation.reservation;
+
+        List<Long> seatIds = reservation.getSeats().stream()
+                .map(ScheduleSeat::getId)  // ScheduleSeat 객체에서 id를 추출합니다.
+                .collect(Collectors.toList());
+
+        Expression<List<Long>> seatIdsExpression = Expressions.constant(seatIds);
+
+        return jpaQueryFactory
+                .select(new QReservationInfoResponse(
+                        qReservation.id,
+                        qTicket.title,
+                        qTicket.content,
+                        qTicket.runningTime,
+                        qSchedule.startTime,
+                        qSchedule.endTime,
+                        seatIdsExpression
+                ))
+                .from(qReservation)
+                .leftJoin(qTicket).on(qReservation.ticketId.eq(qTicket.id))
+                .leftJoin(qSchedule).on(qReservation.scheduleId.eq(qSchedule.id))
+                .where(qReservation.id.eq(reservation.getId()))
+                .fetchOne();
     }
 }
