b9a00f889c859a92496c3a1a262696257b12e659
jung min jun <alswns7984@naver.com>
2024-08-22 00:43:47 +0900

    feat: 예매 상세내역 조회 기능 구현 (#71)

--- src/main/java/kr/doridos/dosticket/domain/reservation/controller/ReservationController.java
+++ src/main/java/kr/doridos/dosticket/domain/reservation/controller/ReservationController.java
@@ -2,6 +2,7 @@ package kr.doridos.dosticket.domain.reservation.controller;
 
 import kr.doridos.dosticket.domain.auth.support.jwt.UserDetailsImpl;
 import kr.doridos.dosticket.domain.reservation.dto.RegisterReservationResponse;
+import kr.doridos.dosticket.domain.reservation.dto.ReservationInfoResponse;
 import kr.doridos.dosticket.domain.reservation.dto.ReservationRequest;
 import kr.doridos.dosticket.domain.reservation.dto.ReservationResponse;
 import kr.doridos.dosticket.domain.reservation.service.ReservationService;
@@ -29,5 +30,11 @@ public class ReservationController {
     @GetMapping("/reservations/me")
     public ResponseEntity<List<ReservationResponse>> findUserReservations(@AuthenticationPrincipal final UserDetailsImpl userDetails) {
         return ResponseEntity.ok(reservationService.findUserReservations(userDetails.getUser().getId()));
+    }
+
+    @GetMapping("/reservations/me/{reservationId}")
+    public ResponseEntity<ReservationInfoResponse> getReservationInfo(@AuthenticationPrincipal final UserDetailsImpl userDetails,
+                                                                      @PathVariable final Long reservationId) {
+        return ResponseEntity.ok(reservationService.getReservationInfo(reservationId, userDetails.getUser().getId()));
     }
 }
--- src/main/java/kr/doridos/dosticket/domain/schedule/repository/ScheduleSeatRepository.java
+++ src/main/java/kr/doridos/dosticket/domain/schedule/repository/ScheduleSeatRepository.java
@@ -3,11 +3,8 @@ package kr.doridos.dosticket.domain.schedule.repository;
 import kr.doridos.dosticket.domain.schedule.entity.ScheduleSeat;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import javax.persistence.LockModeType;
 import java.util.List;
 
 public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, Long> {
