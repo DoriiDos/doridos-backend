62a52cea888a5990e80b3d51b3f2d62800500892
jung min jun <alswns7984@naver.com>
2024-08-24 01:34:29 +0900

    chore: 불필요한 메서드 제거 (#73)

--- src/main/java/kr/doridos/dosticket/domain/user/repository/UserRepository.java
+++ src/main/java/kr/doridos/dosticket/domain/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package kr.doridos.dosticket.domain.user.repository;
 
-import kr.doridos.dosticket.domain.user.User;
+import kr.doridos.dosticket.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,8 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(final String email);
 
     boolean existsByNickname(final String nickname);
-
-    Optional<User> findById(final Long id);
 
     Optional<User> findByEmail(final String email);
 }
