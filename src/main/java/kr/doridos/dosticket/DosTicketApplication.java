1a4d71a233981d823b646dba9544ff6b9b853473
jung min jun <alswns7984@naver.com>
2024-09-10 18:00:09 +0900

    feat: openFeign 사용 가능하도록 설정 (#83)

--- src/main/java/kr/doridos/dosticket/DosTicketApplication.java
+++ src/main/java/kr/doridos/dosticket/DosTicketApplication.java
@@ -2,12 +2,13 @@ package kr.doridos.dosticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DosTicketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DosTicketApplication.class, args);
     }
-
 }
